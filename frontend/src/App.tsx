e635e44a7d7b24e035508bd3e0aabc4c1a4cc122
Karthisri <ksri80734@gmail.com>
2025-04-03 13:20:05 +0530

    private routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AuthForm from './components/formcomponents';
-import UserDashboard from './components/dashboard';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthForm from "./components/formcomponents";
+import UserDashboard from "./components/dashboard";
+import PrivateRoute from "./components/privateroute";
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<AuthForm />} />
+        {/* <Route path="/" element={<AuthForm />} /> */}
         <Route path="/login" element={<AuthForm />} />
-        <Route path="/dashboard" element={<UserDashboard />} />
+        {/* <Route path="/dashboard" element={<UserDashboard />} /> */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<UserDashboard />} />
+        </Route>
       </Routes>
     </Router>
   );
